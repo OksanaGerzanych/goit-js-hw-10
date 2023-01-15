91c9cce938f9e34df187b040537f136c5267789a
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-01-15 15:55:21 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -14,48 +14,50 @@ input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY))
 
 function searchCountry(event) {
     const findCountry = event.target.value.trim();
-    console.log(findCountry)
     if (!findCountry) {
         clearPage();
+        return
     }
     fetchCountries(findCountry)
         .then(country => {
             if (country.length > 10) {
                 Notiflix.Notify.info(
-                    'Too many matches found. Please enter a more specific name.');
+                'Too many matches found. Please enter a more specific name.');
+                clearPage();
                 return;
             } else if (country.length >= 2 && country.length <= 10) {
                 markupCountryList(country);
+                countryInfo.innerHTML = '';
             } else if (country.length === 1) {
                 markupOneCountry(country);
-            }
+                countryList.innerHTML = '';
+            } 
         })
         .catch(error => {
             Notiflix.Notify.failure('Oops, there is no country with that name');
-            clearPage();
+           clearPage();
             return error;
         });
-}
+    }
+
 function markupCountryList(countries) {
     const markup = countries.map(({ name, flags }) => `<li>
         <img src="${flags.svg}" alt="${name}" width="30", height="15">&nbsp${name.official}`)
         .join('');
     countryList.innerHTML = markup;
-}
+};
 
 function markupOneCountry(countries) {
-    const markup = countries.map(({ name,capital,population,flags,languages }) =>
-        `<li>
-        <img src="${flags.svg}" alt="${name}" width="30", height="20">&nbsp<b><BIG>${name.official}</BIG></b>
-        
-        <p><b><span>Capital:</b> </span>${capital}</p>
-        <p><b><span>Population:</b> </span>${population}</p>
-        <p><b><span>Languages:</b> </span>${Object.values(languages).join(",")}</p>
-        </li>`).join('');
+    const markup = countries.map(({ name, capital, population, flags, languages }) => 
+    
+       ` <section><img src="${flags.svg}" alt="${name}" width="30", height="20">&nbsp<b><BIG>${name.official}</BIG></b>
+        <p><span><b>Capital:</b> </span>${capital}</p>
+        <p><span><b>Population:</b> </span>${population}</p>
+        <p><span><b>Languages:</b> </span>${Object.values(languages).join(",")}</p></section>`).join('');
      
-    countryList.innerHTML = markup;
-    console.log(markup)
-}
+    countryInfo.innerHTML = markup;
+    
+};
 
 
 function clearPage() {
